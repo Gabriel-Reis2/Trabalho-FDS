3f40d77e6af7aad67209f5322277d3d177675d28
Gabriel-Reis2 <gabriel.01dosreis@gmail.com>
2025-05-03 16:00:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("mensagem").innerText = 
+      "“Porque sou eu que conheço os planos que tenho para vocês”, diz o Senhor, “planos de fazê-los prosperar e não de causar dano, planos de dar a vocês esperança e um futuro.” - Jeremias 29:11";
+  }
